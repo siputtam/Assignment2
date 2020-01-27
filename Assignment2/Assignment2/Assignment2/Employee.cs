d661c15b5c7213c15a5402aa0f4afd2e32fd6d80
Uttam Kumar Das <59952873+siputtam@users.noreply.github.com>
2020-01-27 16:36:53 +0530

    Add files via upload

---
+++ Assignment2/Assignment2/Assignment2/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+	public abstract class Employee
+	{
+		int id;
+		String emp_name;
+		String desingnation;
+		int emp_expr;
+		int salary;
+	DateTime date;
+		public abstract int Calculate_Salary(int bsal, int expr);
+	}
+
+	public class HR : Employee
+	{
+		public override int Calculate_Salary(int bsal, int expr)
+		{
+			int salary = bsal + (1000 * expr);
+			return salary;
+		}
+	}
+
+	public class Developer : Employee
+	{
+		public override int Calculate_Salary(int bsal, int expr)
+		{
+			int salary = bsal + (2000 * expr);
+			return salary;
+		}
+	}
+}
---
+++ Assignment2/Assignment2/Assignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Employee E1;
+			E1 = new HR();
+			Console.WriteLine(" Enter HR's Basic Salary : ");
+			int bsal = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(" Enter HR's Year of Experience : ");
+			int h_expr = Convert.ToInt32(Console.ReadLine());
+			int sal_hr = E1.Calculate_Salary(bsal, h_expr);
+
+			E1 = new Developer();
+			Console.WriteLine(" Enter Developer's Basic Salary : ");
+			int d_bsal = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(" Enter Developer's Year of Experience : ");
+			int d_expr = Convert.ToInt32(Console.ReadLine());
+			int sal_dev = E1.Calculate_Salary(d_bsal, d_expr);
+
+			Console.WriteLine("The Salary of HR is : " + sal_hr);
+			Console.WriteLine("The Salary of Developer is : " + sal_dev);
+		}
+	}
+
+}
